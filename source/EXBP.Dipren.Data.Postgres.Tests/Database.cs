05c15daa2945ce713bc4fad4870857d1ea643f6c
Christian Jähnert <16286828+chjahnert@users.noreply.github.com>
2023-01-05 23:10:13 +0100

    Implements a set of benchmarking tests for performance optimizations (#18)

---
+++ source/EXBP.Dipren.Data.Postgres.Tests/Database.cs
@@ -0,0 +1,34 @@
+﻿
+using Npgsql;
+
+
+namespace EXBP.Dipren.Data.Postgres.Tests
+{
+    internal static class Database
+    {
+        private const string PATH_SCHEMA_INSTALL_SCRIPT = @"../../../../Database/install.sql";
+        private const string PATH_SCHEMA_REMOVE_SCRIPT = @"../../../../Database/remove.sql";
+
+
+        internal static string ConnectionString => "Host = localhost; Port = 5432; Database = postgres; User ID = postgres; Password = development";
+
+
+        internal static async Task DropDatabaseSchemaAsync(NpgsqlDataSource dataSource, CancellationToken cancellation)
+        {
+            string script = await File.ReadAllTextAsync(PATH_SCHEMA_REMOVE_SCRIPT, cancellation);
+
+            await using NpgsqlCommand command = dataSource.CreateCommand(script);
+
+            await command.ExecuteNonQueryAsync(cancellation);
+        }
+
+        internal static async Task CreateDatabaseSchemaAsync(NpgsqlDataSource dataSource, CancellationToken cancellation)
+        {
+            string script = await File.ReadAllTextAsync(PATH_SCHEMA_INSTALL_SCRIPT, cancellation);
+
+            await using NpgsqlCommand command = dataSource.CreateCommand(script);
+
+            await command.ExecuteNonQueryAsync(cancellation);
+        }
+    }
+}
--- source/EXBP.Dipren.Data.Postgres.Tests/PostgresEngineDataStoreTests.cs
+++ source/EXBP.Dipren.Data.Postgres.Tests/PostgresEngineDataStoreTests.cs
@@ -11,18 +11,12 @@ namespace EXBP.Dipren.Data.Postgres.Tests
     [TestFixture]
     public class PostgresEngineDataStoreTests : EngineDataStoreTests, IDisposable
     {
-        protected const string CONNECTION_STRING = "Host = localhost; Port = 5432; Database = postgres; User ID = postgres; Password = development";
-
-        private const string PATH_SCHEMA_INSTALL_SCRIPT = @"../../../../Database/install.sql";
-        private const string PATH_SCHEMA_REMOVE_SCRIPT = @"../../../../Database/remove.sql";
-
-
         protected NpgsqlDataSource DataSource { get; }
 
 
         public PostgresEngineDataStoreTests()
         {
-            NpgsqlDataSourceBuilder builder = new NpgsqlDataSourceBuilder(CONNECTION_STRING);
+            NpgsqlDataSourceBuilder builder = new NpgsqlDataSourceBuilder(Database.ConnectionString);
 
             this.DataSource = builder.Build();
         }
@@ -35,7 +29,7 @@ namespace EXBP.Dipren.Data.Postgres.Tests
 
 
         protected override Task<IEngineDataStore> OnCreateEngineDataStoreAsync()
-            => Task.FromResult<IEngineDataStore>(new PostgresEngineDataStore(CONNECTION_STRING));
+            => Task.FromResult<IEngineDataStore>(new PostgresEngineDataStore(Database.ConnectionString));
 
         protected override DateTime FormatDateTime(DateTime source)
             => new DateTime(source.Ticks - (source.Ticks % (TimeSpan.TicksPerMillisecond / 1000)), source.Kind);
@@ -44,32 +38,14 @@ namespace EXBP.Dipren.Data.Postgres.Tests
         [SetUp]
         public async Task BeforeEachTestCaseAsync()
         {
-            await this.DropDatabaseSchemaAsync(CancellationToken.None);
-            await this.CreateDatabaseSchemaAsync(CancellationToken.None);
+            await Database.DropDatabaseSchemaAsync(this.DataSource, CancellationToken.None);
+            await Database.CreateDatabaseSchemaAsync(this.DataSource, CancellationToken.None);
         }
 
         [OneTimeTearDown]
         public async Task AfterTestFixtureAsync()
         {
-            await this.DropDatabaseSchemaAsync(CancellationToken.None);
-        }
-
-        private async Task DropDatabaseSchemaAsync(CancellationToken cancellation)
-        {
-            string script = await File.ReadAllTextAsync(PATH_SCHEMA_REMOVE_SCRIPT, cancellation);
-
-            await using NpgsqlCommand command = this.DataSource.CreateCommand(script);
-
-            await command.ExecuteNonQueryAsync(cancellation);
-        }
-
-        private async Task CreateDatabaseSchemaAsync(CancellationToken cancellation)
-        {
-            string script = await File.ReadAllTextAsync(PATH_SCHEMA_INSTALL_SCRIPT, cancellation);
-
-            await using NpgsqlCommand command = this.DataSource.CreateCommand(script);
-
-            await command.ExecuteNonQueryAsync(cancellation);
+            await Database.DropDatabaseSchemaAsync(this.DataSource, CancellationToken.None);
         }
     }
 }
